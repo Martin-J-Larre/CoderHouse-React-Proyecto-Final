28df0b824d4c7afadd238f03a9af949e2237d9ca
Martín Javier Larré <75001420+Martin-J-Larre@users.noreply.github.com>
2021-08-30 21:18:56 -0400

    clase-3 desafío -MENÚ E-COMMERCE

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import  NavBar  from "./components/NavBar";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+          <NavBar />
+      </BrowserRouter>
+      
+    </div>
+  );
+};
 
 export default App;
