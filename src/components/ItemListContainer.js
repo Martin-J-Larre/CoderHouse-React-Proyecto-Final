a4ab8c90990fc7c29c1239addac2848cfde314de
Martín Javier Larré <75001420+Martin-J-Larre@users.noreply.github.com>
2021-08-30 21:53:56 -0400

    clase-5 desafío - CONTADOR CON BOTÓN

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ItemCount from "./ItemCount";
 
 const ItemListContainer = () => {
     return (
          <div>
              <h1>CREA TU LANDING</h1>
+             <ItemCount valorInicial={0} stock={10} />
          </div>
     );
 }
